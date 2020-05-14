cf6a469ed4212731ac24efc3713fc83681916e06
hasan <hasanuli10@gmail.com>
2020-05-14 16:13:30 +0600

    test commented

--- tests/accounting/test_account.py
+++ tests/accounting/test_account.py
@@ -1,4 +1,4 @@
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient
 
 from src.organization.models import Organization
 from src.user.models import User
@@ -26,19 +26,21 @@ def create_admin(username, oid="org1"):
     return organizer
 
 
-class TestAccountViewSet(APITestCase):
-    def setUp(self):
-        self.admin_username = "teacher1"
-        self.admin = create_admin(self.admin_username)
+#
+#
+# class TestAccountViewSet(APITestCase):
+#     def setUp(self):
+#         self.admin_username = "teacher1"
+#         self.admin = create_admin(self.admin_username)
 
-    # def test_create_account(self):
-    #     url = "/api/v1/accounts/?account_group=revenue"
-    #     org = Organization.objects.create(name="test")
-    #     AccountGroup.objects.create(code=15, name="Revenue", category=9)
-    #
-    #     data = {"name": "DabApps"}
-    #     client.force_authenticate(user=self.admin)
-    #     response = self.client.post(url, data, format="json")
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertEqual(Account.objects.count(), 1)
-    #     self.assertEqual(Account.objects.get().name, "DabApps")
+# def test_create_account(self):
+#     url = "/api/v1/accounts/?account_group=revenue"
+#     org = Organization.objects.create(name="test")
+#     AccountGroup.objects.create(code=15, name="Revenue", category=9)
+#
+#     data = {"name": "DabApps"}
+#     client.force_authenticate(user=self.admin)
+#     response = self.client.post(url, data, format="json")
+#     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+#     self.assertEqual(Account.objects.count(), 1)
+#     self.assertEqual(Account.objects.get().name, "DabApps")
