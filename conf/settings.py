deda658bbbbb74a752c1768e9b779fd90b7632b5
Adil Reza <adilreza043@gmail.com>
2020-06-03 23:16:14 +0600

    adding simplejwt token refreshing time 5days for access 7days for refresh

--- conf/settings.py
+++ conf/settings.py
@@ -14,6 +14,9 @@ import os
 
 import environ
 
+# for jwt token configuration
+from datetime import timedelta
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -200,4 +203,20 @@ SWAGGER_SETTINGS = {
     "SECURITY_DEFINITIONS": {
         "JWT": {"type": "apiKey", "name": "Authorization", "in": "header"}
     }
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "JTI_CLAIM": "jti",
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(days=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=7),
 }
