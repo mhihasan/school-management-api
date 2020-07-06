a3ae275b15286d7ec2c45f573fa073926e9b4f08
Adil Reza <adilreza043@gmail.com>
2020-07-06 22:01:40 +0600

    adding new unit test for subject

--- tests/course/conftest.py
+++ tests/course/conftest.py
@@ -1,4 +1,5 @@
-from src.course.models import Course, Section
+from src.course.models import Course, Section, Subject
+from src.employee.models import Employee
 
 
 def course_object(org_id):
@@ -7,3 +8,11 @@ def course_object(org_id):
 
 def section_object(course_id):
     return Section.objects.create(name="class999", course_id=course_id)
+
+
+def teacher_object(email):
+    return Employee.objects.create(email=email, first_name="teacher",gross_salary=89000)
+
+
+def subject_object(name, course, teacher):
+    return Subject.objects.create(name=name, course=course, teacher=teacher)
