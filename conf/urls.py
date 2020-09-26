3e30e5a42fdfc2aeb68aa2c5932e79764de2b77b
Adil Reza <adilreza043@gmail.com>
2020-09-26 21:14:43 +0600

    change the setting file for image upload and view

--- conf/urls.py
+++ conf/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -49,3 +51,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("src.api.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
